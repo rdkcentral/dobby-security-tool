62de38ec931901f0e3c3c897a90d50ece7b0fee1
Ashoks-hub <79261622+Ashoks-hub@users.noreply.github.com>
2022-06-06 12:18:39 +0530

    Fix for the issue #7

--- tests/5_dobby_container_runtime_test.sh
+++ tests/5_dobby_container_runtime_test.sh
@@ -61,24 +61,20 @@ test_5_3() {
 	
 	status=$(cat /proc/$Container_PID/status | grep CapPrm | awk '{print $2}')
 	output=$(capsh --decode=$status | sed 's/.*=//g')
+	
 	input=( cap_net_raw cap_dac_read_search cap_sys_module cap_sys_admin cap_sys_ptrace )
 	IFS=','
+	
 	read -a ouputarr <<<"$output"
+	output_length=${#ouputarr[@]}
+	input_length=${#input[@]}
+	
 	#accessing each element of array
-	for i in "${!ouputarr[@]}";
+	for (( i=0; i<output_length; i++ ));
 	do
-		for j in "${!input[@]}";
+		for (( j=0; j<input_length; j++ ));
 		do
-			if [ "${input[$j]}" == "${ouputarr[$i]}" ]; then
-				fail "$check"
-				return
-			elif [ "${input[$j]}" == "${ouputarr[$i]}" ]; then
-				fail "$check"
-				return
-			elif [ "${input[$j]}" == "${ouputarr[$i]}" ]; then
-				fail "$check"
-				return
-			elif [ "${input[$j]}" == "${ouputarr[$i]}" ]; then
+			if [ "${ouputarr[$i]}" == "${input[$j]}" ]; then
 				fail "$check"
 				return
 			fi
